c1b30b3ea535ade4924468f644eab84ede806de7
Antoni Segura Puimedon <toni@midokura.com>
2015-11-26 02:22:32 +0100

    fix unattended installation of ansible

--- scripts/allinone.sh
+++ scripts/allinone.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install ansible
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get -y update
+sudo apt-get -y install ansible
 
 curl -sL quickstart.midonet.org | sudo bash
