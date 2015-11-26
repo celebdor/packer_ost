0a4b7c705d9fbe45065f288158c10a331d6fa3da
Antoni Segura Puimedon <toni@midokura.com>
2015-11-26 02:22:07 +0100

    zero disks only for vmware and vbox

--- scripts/zerodisk.sh
+++ scripts/zerodisk.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+VIRT=$(facter virtual)
+
+if [[ "$VIRT" != "vmware" ]] || [[ "$VIRT" != "virtualbox" ]]; then
+    exit 0
+fi
+
 # Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
